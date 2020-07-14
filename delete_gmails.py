cbf9e456be2038740fac489b0229e8834157eb28
rajke24 <jarek.andrzejewski99@gmail.com>
2020-07-14 22:38:24 +0200

    add script that deletes unnecessary mails

---
+++ delete_gmails.py
@@ -0,0 +1,25 @@
+from imapclient import IMAPClient
+from datetime import datetime
+
+def delete_msg(unseen_for_days=0):
+    mailbox = IMAPClient('imap.gmail.com', ssl=True, port=993)
+    mailbox.login('your@mail.com', 'password')
+    inbox_mails = mailbox.select_folder('INBOX')
+    print('You have total number %d of mails in your inbox' % inbox_mails[b'EXISTS'])
+    seen_msg = mailbox.search('SEEN')
+    mailbox.delete_messages(seen_msg)
+    print(f'{len(seen_msg)} already seen messages has been deleted')
+    unseen_msg = mailbox.search('UNSEEN')
+    no_unseen_deleted = 0
+    unseen_del = []
+    for id, data in mailbox.fetch(unseen_msg, ['INTERNALDATE']).items():
+        date_of_recipt = data[b'INTERNALDATE']
+        delta = (datetime.now() - date_of_recipt).days
+        if delta > unseen_for_days:
+            unseen_del.append(id)
+            no_unseen_deleted += 1
+    if no_unseen_deleted > 0:
+        mailbox.delete_messages(unseen_del)
+
+    print(f'{no_unseen_deleted} unseen_del for more than {unseen_for_days} days mails deleted.')
+    mailbox.logout()
